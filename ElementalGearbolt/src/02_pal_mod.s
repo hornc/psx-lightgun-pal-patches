77a3800d415309254a33d716204a2c2c4466c8fc
Charles Horn <charles.horn@gmail.com>
2026-06-28 16:56:11 +1200

    rewrite memcard default calibration for PAL offsets Now has GunCon and Justifier equally centered on emulator ...

--- ElementalGearbolt/src/02_pal_mod.s
+++ ElementalGearbolt/src/02_pal_mod.s
@@ -2,12 +2,25 @@
 .set noreorder
 .set noat
 # Write data to force PAL mode to 0x8005_12e8
-lui $t0, 0x8005
+lui $at, 0x8005
 li  $t1, 0x01
-sb  $t1, 0x12e8($t0)
-# Write 0xffff_ffc0 to 0x8005_29ec OR write 0xffc0 hw to 0x8002_57f0
-lui $t0, 0x8002
-li  $t2, 0xffc0
-sh  $t2, 0x57f0($t0)
+sb  $t1, 0x12e8($at)
+# GunCon / base X/Y in memcard adjust:
+lui $at, 0x8002
+li $t0, 0x24092207  # "addiu	$t1, $0, 0x2207"
+sw $t0, 0x7a8c($at)
+li $t1, 0xa429      # change sb to sh, and r0 to t1
+sh $t1, 0x7a92($at)
+sw $0, 0x7a98($at)  # clear sb $r0 -> ..f5
+# Justifier X/Y adjust:
+# Justifier X: 0x8005_29e4, set via hw at 0x8002_57e8
+# Justifier Y: 0x8005_29ec, set via hw at 0x8002_57f0
+li  $t1, 0xfff4
+li  $t2, 0xffe0
+sh  $t1, 0x57e8($at)
+sh  $t2, 0x57f0($at)
+# Justifier only offset fix:
+#li  $t1, 0xffc0
+#sh  $t1, 0x57f0($at)
 jr  $ra
 nop
