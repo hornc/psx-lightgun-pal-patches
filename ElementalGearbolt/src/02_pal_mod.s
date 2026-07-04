953af0747688ee0518801572c4711749fc19c444
Charles Horn <charles.horn@gmail.com>
2026-07-04 22:03:08 +1200

    comment location

--- ElementalGearbolt/src/02_pal_mod.s
+++ ElementalGearbolt/src/02_pal_mod.s
@@ -7,10 +7,10 @@ li  $t1, 0x01
 sb  $t1, 0x12e8($at)
 # Shift GP1(0x07)
 lui $at, 0x8003
-li  $t0, 0x26420007  # "addiu $v0, $s2, 0x7"
-li  $t1, 0x26240007  # "addiu $a0, $s1, 0x7"
-sw  $t0, -0xf68($at)
-sw  $t1, -0xf60($at)
+li  $t0, 0x26420007   # "addiu $v0, $s2, 0x7"
+li  $t1, 0x26240007   # "addiu $a0, $s1, 0x7"
+sw  $t0, -0xf68($at)  # -> 0x8002_f098
+sw  $t1, -0xf60($at)  # -> 0x8002_f0a0
 # GunCon / base X/Y in memcard adjust:
 lui $at, 0x8002
 li  $t0, 0x24092907  # "addiu	$t1, $0, 0x2907"
