782285f33f475ecb1765bf79c09787cd5019b9d3
Charles Horn <charles.horn@gmail.com>
2026-07-25 00:57:17 +1200

    validate patcher.sh required PSX-EXE arg

--- LSD/patcher.sh
+++ LSD/patcher.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
-origfile=$1  # PSX EXE!
+origfile="$1"  # PSX EXE!
 outfile=${origfile}_mod
 origsha="76322eeade5ebb22dca57fdeac7d68c30f06308d"
 
 source ../scripts/common.sh
 
-echo Using asm in src/ to patch $origfile to $outfile
-echo "$origsha  $origfile" | shasum --check  # en translation of LSD - Dream Emulator (Japan) SLPS_015.56
-
-apply_patch_src $origfile $outfile
-
+require_target $origfile
+apply_patch_src $origfile $outfile $origsha
 
 echo Inject modifed PS-X EXE back into .bin:
 psxinject "LSD - Dream Emulator (Japan).bin" $origfile $outfile
