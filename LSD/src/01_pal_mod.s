90f06c5fa61415d1d62d2d3bf4027f44f52dfdf3
Charles Horn <charles.horn@gmail.com>
2026-07-26 21:09:04 +1200

    change y1 directly

--- LSD/src/01_pal_mod.s
+++ LSD/src/01_pal_mod.s
@@ -6,7 +6,3 @@
 lui $at, 0x8002
 li  $a0, 0x09
 sb  $a0, 0x1b70($at)
-# set correct PAL screen offset:
-sb  $t5, 0x1918($at)
-# t5 happens to be set to 0x2B, which is exactly what we need to change
-# GP1(07h) y1 to, from 0x10. The existing code adds 240 to y1 to get y2
--- LSD/src/03_screen_y2.s
+++ LSD/src/03_screen_y2.s
@@ -2,4 +2,4 @@
 .set noreorder
 .set noat
 # setting GP1(07h) y2:
-ori $v1, $0, 0x11b
+ori $v1, $0, 0x11b  # was 0x100 for NTSC
