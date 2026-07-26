4d29580ba9103892ee2e15621e23f483b992464f
Charles Horn <charles.horn@gmail.com>
2026-07-26 20:57:37 +1200

    centered PAL screen

--- LSD/src/01_pal_mod.s
+++ LSD/src/01_pal_mod.s
@@ -1,10 +1,12 @@
 .org 0x21c4
 .set noreorder
 .set noat
+# v0.2 PAL mode with centered screen
 # Set PAL mode:
 lui $at, 0x8002
 li  $a0, 0x09
 sb  $a0, 0x1b70($at)
-# v0.1 minimal PAL mode
-# TODO: lower the image as it sits
-# right at the top of the screen
+# set correct PAL screen offset:
+sb  $t5, 0x1918($at)
+# t5 happens to be set to 0x2B, which is exactly what we need to change
+# GP1(07h) y1 to, from 0x10. The existing code adds 240 to y1 to get y2
---
+++ LSD/src/03_screen_y2.s
@@ -0,0 +1,5 @@
+.org 0x12238 
+.set noreorder
+.set noat
+# setting GP1(07h) y2:
+ori $v1, $0, 0x11b
