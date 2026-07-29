9a54d3280f1741eea5151d8303a6c0515c597dac
Charles Horn <charles.horn@gmail.com>
2026-07-30 01:55:47 +1200

    adjust Justifier y-offset

--- ProjectHornedOwl/src/02_pal_mod.s
+++ ProjectHornedOwl/src/02_pal_mod.s
@@ -5,8 +5,14 @@
 # the check at 8003_fc00
 lui $at, 0x8004
 sw  $0, -0x400($at)
+# Set Justifier Y-offset
+# hw at 0x8008_aac4 = 0xYYXX
+# for un-shifted screen, y offset = 0x42
+lui $at, 0x8009
+li  $t1, 0x4200
+sh  $t1, -0x553c($at)
 # FlushCache() because we modified in mem instructions:
 li  $t0, 0xa0
 li  $t1, 0x44
 jr  $t0  # $ra is already set from the code we hijacked
-nop
+nop      # FlushCache() will return to the main program
