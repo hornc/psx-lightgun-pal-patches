9400d8c428e804e2d31fc1794ea2ad1588d01a60
Charles Horn <charles.horn@gmail.com>
2026-08-01 18:43:12 +1200

    add flag to manually adjust Screen Y if needed in future

--- ElementalGearbolt/src/02_pal_mod.s
+++ ElementalGearbolt/src/02_pal_mod.s
@@ -1,10 +1,24 @@
 .org 0x0c04
 .set noreorder
 .set noat
+.set MANUAL_YSCREEN, 0
+
 # Write data to force PAL mode to 0x8005_12e8:
 lui $at, 0x8005
 li  $t1, 0x01
 sb  $t1, 0x12e8($at)
+
+.if MANUAL_YSCREEN
+    # Adjust GP1(0x07) further, if needed.
+    # defaults to y1: 43, y2: 283 in PAL
+    #             y1: 16, y2: 256 in NTSC
+    lui $at, 0x8003
+    li  $t0, 0x26420007   # "addiu $v0, $s2, 0x7", inc. y1 by 7
+    li  $t1, 0x26240007   # "addiu $a0, $s1, 0x7", inc. y2 by 7
+    sw  $t0, -0xf68($at)  # -> 0x8002_f098
+    sw  $t1, -0xf60($at)  # -> 0x8002_f0a0
+.endif
+
 # GunCon Adjust:
 # base Y:00 -> 22, X:02 -> 07 in memcard save:
 lui $at, 0x8002
